366d66e28260fb82679d6670aa601163f7606852
Gontxal0 <gggonzalez002@ikasle.ehu.es>
2015-06-15 12:55:48 +0200

    primer commit, edicion del registro de usuario

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/student_controller.js
@@ -0,0 +1,9 @@
+//GET /controllers/student
+exports.new = function(req, res) {
+    res.render('student/studentRegistration');
+    //res.write("Hola");
+    };
+
+exports.create = function(req,res) {
+    
+    };
