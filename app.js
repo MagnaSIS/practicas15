67cd8616e39b5ad391634aa9c169fc1eb135604f
hantruejo <hector.antruejo@gmail.com>
2015-06-15 10:44:46 +0200

    Routing Changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+/*
+ * Required Variables
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+ * Define Controllers
+ */
+var sessionController	=	require	('../controllers/session_controller');
+var studentController	=	require	('../controllers/student_controller');
+var managerController	=	requite ('../controllers/manage_controller');
+var courseController	=	require	('../controllers/course_controller');
+var calcsController		=	require ('../controllers/course_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+/*
+*	Session Controller
+*/
+router.get('/login',		sessionController.new);
+router.post('/login',		sessionController.create);
+router.delete('/logout',	sessionController.loginRequired,	sessionController.destroy);
+
+/*
+*	Students Controller
+*/
+router.get('/students,',		studentController.new);
+router.post('/students',		studentController.create);
+router.delete('/students/:id',	sessionController.isStudent,	studentController.destroy);
+router.put('/students/:id',		sessionController.isStudent,	studentController.update);
+
+/*
+*	Manager Controller
+*/
+router.get('/manager',			sessionController.isAdmin,	managerController.new);
+router.post('/manager',			sessionController.isAdmin,	managerController.create);
+router.delete('/manager/:id',	sessionController.isAdmin,	managerController.destroy);
+router.put('/manager/:id',		sessionController.isAdmin,	managerController.update);
+
+/*
+ * Admin Controller
+ */
+router.get('/course',			sessionController.isManager,	courseController.new);
+router.post('/course',			sessionController.isManager,	courseController.create);
+router.delete('/course/:id',	sessionController.isManager,	courseController.destroy);
+router.put('/course/:id',		sessionController.isManager,	courseController.update);
+
+
+/*
+* Calcs controller
+*/
+router.get('/calcs/:idstudent/:idcourse',	sessionController.loginRequired,	calcsController.new);
