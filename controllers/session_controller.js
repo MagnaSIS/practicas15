f3316977f6d5db59878514452ed5498333ee179f
Gontxal0 <gggonzalez002@ikasle.ehu.es>
2015-06-17 08:58:32 +0200

    reparacion del error de borrar session_controler gracias a manolo

---
+++ controllers/session_controller.js
@@ -0,0 +1,86 @@
+/**  
+ *   placeForMe -
+ *   Copyright (C) 2015 by Magna SIS <magnasis@magnasis.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var models=require("../models/models.js");
+var util=require("../includes/utilities.js");
+
+//Check if user is login
+exports.loginRequired = function (req,res,next){
+		if (req.session.user){
+			next();
+		}else{
+			res.redirect('login');
+		}
+};
+
+//Check if user is Student
+exports.isStudent = function (req,res,next){
+		if (req.session.role === "STUDENT"){
+			next();
+		}else{
+			new Error("Permiso denegado.")
+		}
+};
+
+//Check if user is Manager
+exports.isManager = function (req,res,next){
+		if (req.session.role === "MANAGER"){
+			next();
+		}else{
+			new Error("Permiso denegado.")
+		}
+};
+
+//Check if user is Admin
+exports.isAdmin = function (req,res,next){
+		if (req.session.role === "ADMIN"){
+			next();
+		}else{
+			new Error("Permiso denegado.")
+		}
+};
+
+
+//Get /login Login Form
+exports.new = function(req,res){
+	var errors=req.session.errors || {};
+	req.session.errors={};
+	res.render('session/login', {errors : errors});
+};
+
+//Post /login Login check
+exports.create = function(req,res){
+	models.User.find({where: {email: req.body.login, password: util.encrypt(req.body.password)}}).then(function(user) {
+		if (user) {	
+			req.session.user = {email: user.email, role: user.role};			
+		} else{
+			req.session.errors =[{"message": 'Usuario o contraseña incorrectas'}];
+		}
+		res.redirect("/login");	
+	}).catch(function(error){
+		req.session.errors =[{"message": 'Usuario o contraseña incorrectas'} , {"message":("error: " + error) || ""}];	
+		res.redirect("/login");	
+	});
+	
+};
+	
+//Delete /logout session destroy
+exports.destroy = function (req,res){
+	delete req.session.user;
+	res.redirect("/");
+};
